8e7067195431a558d89cbc48c99153bad4c99477
Charlie Birks <charlie@daft.games>
2023-07-10 16:01:26 +0100

    minimal picovision board
    Doesn't work but does get the example to start

---
+++ minsdl/board/PicoVision.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Board.h"
+
+class PIO;
+
+class PicoVisionBoard final : public Board
+{
+public:
+    PicoVisionBoard(MemoryBus &mem);
+
+    void getScreenSize(int &w, int &h) override;
+    int getScreenFormat() override;
+    const uint8_t *getScreenData() override;
+
+    bool hasAudio() override {return false;}
+    int getNumAudioSamples() override {return 0;}
+    int16_t getAudioSample() override {return 0;}
+
+    void handleEvent(SDL_Event &event) override {}
+    void update(uint64_t time) override {}
+
+private:
+    MemoryBus &mem;
+
+    uint16_t screenData[640 * 480];
+
+    void onPIOUpdate(uint64_t time, PIO &pio);
+};
