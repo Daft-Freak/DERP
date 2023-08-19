006917f1ae356368b8b1e0835a82ea566ebbd760
Charlie Birks <charlie@daft.games>
2023-08-19 23:19:47 +0100

    "handle" some more PIO regs
    Makes log less noisy

--- core/PIO.cpp
+++ core/PIO.cpp
@@ -75,14 +75,56 @@ uint32_t PIO::regRead(uint32_t addr)
 {
     switch(addr)
     {
+        case PIO_CTRL_OFFSET:
+            return hw.ctrl;
+
         case PIO_FSTAT_OFFSET:
             return PIO_FSTAT_TXEMPTY_BITS | PIO_FSTAT_RXEMPTY_BITS; // all FIFOs empty
         case PIO_FDEBUG_OFFSET:
             return PIO_FDEBUG_TXSTALL_BITS; // all TXSTALL
+
+        case PIO_SM0_CLKDIV_OFFSET:
+        case PIO_SM1_CLKDIV_OFFSET:
+        case PIO_SM2_CLKDIV_OFFSET:
+        case PIO_SM3_CLKDIV_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            return hw.sm[sm].clkdiv;
+        }
+
+        case PIO_SM0_EXECCTRL_OFFSET:
+        case PIO_SM1_EXECCTRL_OFFSET:
+        case PIO_SM2_EXECCTRL_OFFSET:
+        case PIO_SM3_EXECCTRL_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            return hw.sm[sm].execctrl;
+        }
+
+        case PIO_SM0_SHIFTCTRL_OFFSET:
+        case PIO_SM1_SHIFTCTRL_OFFSET:
+        case PIO_SM2_SHIFTCTRL_OFFSET:
+        case PIO_SM3_SHIFTCTRL_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            return hw.sm[sm].shiftctrl;
+        }
+
         case PIO_SM0_ADDR_OFFSET:
         {
             static int counter = 0;
             return counter++ & PIO_SM0_ADDR_BITS;
+        }
+
+        // INSTR
+
+        case PIO_SM0_PINCTRL_OFFSET:
+        case PIO_SM1_PINCTRL_OFFSET:
+        case PIO_SM2_PINCTRL_OFFSET:
+        case PIO_SM3_PINCTRL_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            return hw.sm[sm].pinctrl;
         }
     }
 
@@ -99,8 +141,102 @@ void PIO::regWrite(uint32_t addr, uint32_t data)
 
     switch(addr)
     {
+        case PIO_CTRL_OFFSET:
+        {
+            auto oldVal = hw.ctrl;
+            if(updateReg(hw.ctrl, data, atomic))
+            {
+                auto enabled = (oldVal ^ hw.ctrl) & hw.ctrl & PIO_CTRL_SM_ENABLE_BITS;
+                for(unsigned i = 0; i < NUM_PIO_STATE_MACHINES; i++)
+                {
+                    if(enabled & (1 << (PIO_CTRL_SM_ENABLE_LSB + i)))
+                        logf(LogLevel::Debug, logComponent, "%i SM%i enabled CLKDIV %08X EXECCTRL %08X SHIFTCTRL %08X PINCTRL %08X", index, i, hw.sm[i].clkdiv, hw.sm[i].execctrl, hw.sm[i].shiftctrl, hw.sm[i].pinctrl);
+                }
+            }
+            return;
+        }
         case PIO_TXF0_OFFSET:
             return;
+
+        case PIO_INSTR_MEM0_OFFSET:
+        case PIO_INSTR_MEM1_OFFSET:
+        case PIO_INSTR_MEM2_OFFSET:
+        case PIO_INSTR_MEM3_OFFSET:
+        case PIO_INSTR_MEM4_OFFSET:
+        case PIO_INSTR_MEM5_OFFSET:
+        case PIO_INSTR_MEM6_OFFSET:
+        case PIO_INSTR_MEM7_OFFSET:
+        case PIO_INSTR_MEM8_OFFSET:
+        case PIO_INSTR_MEM9_OFFSET:
+        case PIO_INSTR_MEM10_OFFSET:
+        case PIO_INSTR_MEM11_OFFSET:
+        case PIO_INSTR_MEM12_OFFSET:
+        case PIO_INSTR_MEM13_OFFSET:
+        case PIO_INSTR_MEM14_OFFSET:
+        case PIO_INSTR_MEM15_OFFSET:
+        case PIO_INSTR_MEM16_OFFSET:
+        case PIO_INSTR_MEM17_OFFSET:
+        case PIO_INSTR_MEM18_OFFSET:
+        case PIO_INSTR_MEM19_OFFSET:
+        case PIO_INSTR_MEM20_OFFSET:
+        case PIO_INSTR_MEM21_OFFSET:
+        case PIO_INSTR_MEM22_OFFSET:
+        case PIO_INSTR_MEM23_OFFSET:
+        case PIO_INSTR_MEM24_OFFSET:
+        case PIO_INSTR_MEM25_OFFSET:
+        case PIO_INSTR_MEM26_OFFSET:
+        case PIO_INSTR_MEM27_OFFSET:
+        case PIO_INSTR_MEM28_OFFSET:
+        case PIO_INSTR_MEM29_OFFSET:
+        case PIO_INSTR_MEM30_OFFSET:
+        case PIO_INSTR_MEM31_OFFSET:
+        {
+            int off = (addr - PIO_INSTR_MEM0_OFFSET) / 4;
+            updateReg(hw.instr_mem[off], data, atomic);
+            return;
+        }
+
+        case PIO_SM0_CLKDIV_OFFSET:
+        case PIO_SM1_CLKDIV_OFFSET:
+        case PIO_SM2_CLKDIV_OFFSET:
+        case PIO_SM3_CLKDIV_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            updateReg(hw.sm[sm].clkdiv, data, atomic);
+            return;
+        }
+
+        case PIO_SM0_EXECCTRL_OFFSET:
+        case PIO_SM1_EXECCTRL_OFFSET:
+        case PIO_SM2_EXECCTRL_OFFSET:
+        case PIO_SM3_EXECCTRL_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            updateReg(hw.sm[sm].execctrl, data, atomic);
+            return;
+        }
+
+        case PIO_SM0_SHIFTCTRL_OFFSET:
+        case PIO_SM1_SHIFTCTRL_OFFSET:
+        case PIO_SM2_SHIFTCTRL_OFFSET:
+        case PIO_SM3_SHIFTCTRL_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            updateReg(hw.sm[sm].shiftctrl, data, atomic);
+            return;
+        }
+
+        // ADDR, INSTR
+
+        case PIO_SM0_PINCTRL_OFFSET:
+        case PIO_SM1_PINCTRL_OFFSET:
+        case PIO_SM2_PINCTRL_OFFSET:
+        case PIO_SM3_PINCTRL_OFFSET:
+        {
+            int sm = (addr - PIO_SM0_CLKDIV_OFFSET) / (PIO_SM1_CLKDIV_OFFSET - PIO_SM0_CLKDIV_OFFSET);
+            updateReg(hw.sm[sm].pinctrl, data, atomic);
+            return;
+        }
     }
 
     logf(LogLevel::NotImplemented, logComponent, "%i W %04X%s%08X", index, addr, op[atomic], data);
