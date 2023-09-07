08aaa85c8600b102d0eb3d62566d5224bf8726ed
Charlie Birks <charlie@daft.games>
2023-09-07 16:47:01 +0100

    PicoVision ram writes

--- minsdl/board/PicoVision.cpp
+++ minsdl/board/PicoVision.cpp
@@ -14,7 +14,7 @@ PicoVisionBoard::PicoVisionBoard(MemoryBus &mem) : mem(mem)
     mem.getGPIO().clearInputFloatingMask(1 << 16); // vsync
     mem.getGPIO().setInputMask(1 << 16); // TODO
 
-    //mem.getPIO(1).setUpdateCallback([this](auto time, auto &pio){onPIOUpdate(time, pio);});
+    mem.getPIO(1).setUpdateCallback([this](auto time, auto &pio){onPIOUpdate(time, pio);});
 }
 
 void PicoVisionBoard::getScreenSize(int &w, int &h)
@@ -37,5 +37,67 @@ const uint8_t *PicoVisionBoard::getScreenData()
 
 void PicoVisionBoard::onPIOUpdate(uint64_t time, PIO &pio)
 {
-  
+    auto &txFifo = pio.getTXFIFO(0);
+    //auto &smHW = pio.getHW().sm[0];
+
+    if(txFifo.empty())
+        return;
+
+    int ramBank = (mem.getGPIO().getPadState() & (1 << 8)) ? 1 : 0;
+
+    while(!txFifo.empty())
+    {
+        auto data = txFifo.pop();
+
+        bool isResetProg = !ramQuadEnabled[ramBank]; // assume reset program until quad enabled
+
+        if(ramCmdOffset[ramBank] == 0)
+        {
+            if(isResetProg)
+                ramCmdLenWords[ramBank] = data / 32 + 1;
+            else
+                ramCmdLenWords[ramBank] = (data + 1) * (ramQuadEnabled[ramBank] ? 4 : 1) / 32 + 3;
+
+            ramDataLenBytes[ramBank] = (data + 1) * (ramQuadEnabled[ramBank] ? 4 : 1) / 8;
+        }
+        else if(ramCmdOffset[ramBank] == 1)
+        {
+            auto cmd = ramCommand[ramBank] = data >> 24;
+
+            if(cmd == 0x35)
+            {
+                logf(LogLevel::Debug, logComponent, "psram %i quad enabled", ramBank);
+                ramQuadEnabled[ramBank] = true;
+            }
+            else if(cmd == 0x38) // write
+                ramCmdAddr[ramBank] = data & 0xFFFFFF;
+            else
+                logf(LogLevel::Debug, logComponent, "psram %i cmd %02X %06X len %i", ramBank, cmd, data & 0xFFFFFF, ramDataLenBytes[ramBank]);
+        }
+        else if(ramCmdOffset[ramBank] == 2 && !isResetProg)
+        {
+            // pio addr
+        }
+        else // data
+        {
+            if(ramCommand[ramBank] == 0x38) // write
+            {
+                auto offset = (ramCmdOffset[ramBank] - 3) * 4;
+
+                for(int i = 0; i < std::min(4, ramDataLenBytes[ramBank] - offset); i++)
+                {
+                    uint32_t addr = ramCmdAddr[ramBank] + offset + i;
+
+                    psramData[ramBank][addr] = data >> 24;
+
+                    data <<= 8;
+                }
+            }
+        }
+
+        if(ramCmdOffset[ramBank] == ramCmdLenWords[ramBank])
+            ramCmdOffset[ramBank] = 0;
+        else
+            ramCmdOffset[ramBank]++;
+    }
 }
--- minsdl/board/PicoVision.h
+++ minsdl/board/PicoVision.h
@@ -22,5 +22,15 @@ private:
 
     uint16_t screenData[640 * 480];
 
+    int ramCmdOffset[2] = {0, 0};
+    int ramCmdLenWords[2] = {0, 0};
+    int ramDataLenBytes[2] = {0, 0};
+    uint8_t ramCommand[2] = {0, 0};
+    uint32_t ramCmdAddr[2] = {0, 0};
+
+    bool ramQuadEnabled[2] = {0, 0};
+
+    uint8_t psramData[2][8 * 1024 * 1024];
+
     void onPIOUpdate(uint64_t time, PIO &pio);
 };
